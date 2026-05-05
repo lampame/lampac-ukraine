cfdf0f2d769beeeabdf228a46058c93cb32b3514
Felix <midi2001@mail.ru>
2026-05-05 21:50:50 +0300

    refactor(playerjs): rename PlayerJsDecoder namespace to LME.Common.Playerjs
    Move the PlayerJsDecoder class from Shared.Engine to LME.Common.Playerjs
    namespace to align with the common library structure. Update global usings
    in GlobalUsings.cs and add references to PlayerJsDecoder.cs in module
    manifest files to reflect the new namespace location.

--- LME.Shared/Playerjs/PlayerJsDecoder.cs
+++ LME.Shared/Playerjs/PlayerJsDecoder.cs
@@ -7,7 +7,7 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.RegularExpressions;
 
-namespace Shared.Engine
+namespace LME.Common.Playerjs
 {
     public static class PlayerJsDecoder
     {
